160cf2b3337b13573d343649004a64798a1c9a93
Florian Marending <florian@marending.dev>
2026-04-14 21:54:13 +0200

    Rename dashboard.ino

--- dashboard/compile.sh
+++ dashboard/compile.sh
@@ -1,1 +1,1 @@
-arduino-cli compile --fqbn esp32:esp32:esp32s3:PartitionScheme=huge_app,PSRAM=opi count.ino
+arduino-cli compile --fqbn esp32:esp32:esp32s3:PartitionScheme=huge_app,PSRAM=opi dashboard.ino
--- dashboard/upload.sh
+++ dashboard/upload.sh
@@ -1,1 +1,1 @@
-arduino-cli upload -p /dev/cu.usbserial-110 --fqbn esp32:esp32:esp32s3:PartitionScheme=huge_app,PSRAM=opi,UploadSpeed=115200 count.ino
+arduino-cli upload -p /dev/cu.usbserial-110 --fqbn esp32:esp32:esp32s3:PartitionScheme=huge_app,PSRAM=opi,UploadSpeed=115200 dashboard.ino
